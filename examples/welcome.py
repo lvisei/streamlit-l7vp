eeafc4e048475de3530413e8d3fe73afb7456795
yunji <yunji.me@outlook.com>
2024-03-28 11:34:15 +0800

    chore: docs

--- examples/welcome.py
+++ examples/welcome.py
@@ -10,5 +10,3 @@ st.set_page_config(page_title="streamlit-l7vp: AntV L7VP for Streamlit!", page_i
 
 This project was created to allow us to render [L7VP](https://github.com/antvis/L7VP) maps in streamlit. In the meantime, below are some examples of what you can do with just a few lines of code:
 """
-
-st.image("https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*_GfqQoRCqQkAAAAAAAAAAAAADmJ7AQ/fmt.webp")
